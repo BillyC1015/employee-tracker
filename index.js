d3dd397a3fec94905c9ae5f6e9e793565db7991a
Billy Cody <william.cody1015@gmail.com>
2023-06-05 11:49:38 -0400

    added main inquirer prompt

---
+++ index.js
@@ -0,0 +1,55 @@
+const inquirer=require("inquirer")
+const connection=require("./connection")
+
+
+function startApp() {
+    inquirer.prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a new department",
+          "Add a new role",
+          "Add a new employee",
+          "Update employee role",
+          "Exit"
+        ]
+      }])
+      .then(response => {
+        switch (response.action) {
+          case "View all departments":
+            viewDepartments();
+            break;
+          case "View all roles":
+            viewRoles();
+            break;
+          case "View all employees":
+            viewEmployees();
+            break;
+          case "Add a new department":
+            addDepartment();
+            break;
+          case "Add a new role":
+            addRole();
+            break;
+          case "Add a new employee":
+            addEmployee();
+            break;
+          case "Update employee role":
+            updateEmpRole();
+            break;
+          case "exit":
+            connection.end();
+            break;
+        }
+      });
+  };
+
+// functions for view queries 
+
+
+  startApp()
